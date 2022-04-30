d0fa67db146fd4e5ef3576b05f8d2359f655a888
elzp <piechelzbieta@wp.pl>
2022-04-30 18:42:16 +0200

    modal not centered

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Import stylesheets
 import './style.css';
 
+const modal = document.querySelector('.main-modal');
+const exit = document.querySelector('.fill-current');
+const button = document.querySelector('body>div>button');
+
+exit.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+button.addEventListener('click', () => {
+  modal.style.display = 'block';
+  modal.style.margin = 'auto';
+});
